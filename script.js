c75566d8233a800fdbef393621210a57d1c581fe
daligofor <dalilammouchi70@gmail.com>
2025-04-29 00:30:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDetails() {
+    const details = document.getElementById('details1920');
+    details.style.display = (details.style.display === 'none' || details.style.display === '') ? 'block' : 'none';
+}
